b81a77574e65a5b4e18c0a76bed398b810b6a449
stepforbes <step@greengraphics.com.au>
2014-09-01 16:47:11 +1000

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="clearer">
+	
+	<div class="footer-credits">
+
+	<p>
+	All content &copy; <a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a> <?php echo date('Y'); ?> | <a href="<?php bloginfo('home'); ?>/privacy-policy">Privacy Policy</a> | <a href="<?php bloginfo('home'); ?>/disclaimer">Disclaimer</a> | <a href="<?php bloginfo('home'); ?>">Other Stuff</a> 
+	</p>
+
+	</div>
+
+	<?php wp_footer(); ?>
+
+</footer><!--end footer-->
+
+</div><!--end wrapper-->
+</body>
+</html>
