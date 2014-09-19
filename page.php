c89cb0ac81fd33dc0f4aebd0cff7eb1174fb3591
Step Forbes <stepforbes@users.noreply.github.com>
2014-09-19 10:03:20 +1000

    Update page.php
    fixed page not found wording...

--- page.php
+++ page.php
@@ -14,13 +14,14 @@
 
 <?php endwhile; else : ?>
 	
-	<article class="content">
-		<h2>Not Found</h2>
-		<p>Sorry we can't find what anything that matches your search.</p>
-		<p>You could try another search or browse our categories.</p>
-		<p><?php get_search_form(); ?></p>
-		<p><ul><?php wp_list_categories('title_li=<h2>Categories</h2>'); ?></ul><p
-	</article><!--end content-->
+	<article class="entry">
+		<h2>Page not found</h2>
+		<div class="the_content">
+		<p>Sorry we can't find that.</p>
+		<p>You could try another search or <a href="<?php bloginfo('home'); ?>">return to our homepage</a>.</p>
+		<?php get_search_form(); ?>
+		</div><!--end the_content-->
+	</article><!--end entry-->
 
 <?php endif; ?>
 
