a0359fc5e3fcf987da629073609e7d6474401573
Step Forbes <stepforbes@users.noreply.github.com>
2014-09-19 11:00:17 +1000

    Update index.php
    take <article> out of the loop (so it doesn't repeat).

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 
 <section id="content">
 
+	<article class="entry">
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
-	<article class="entry">
-	
+
 		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1> 
 	
 		<div class="the_content">
@@ -14,20 +15,19 @@
 		
 		</div><!--end the_content-->
 		
-	</article><!--end entry-->
 
 <?php endwhile; else : ?>
 	
-	<article class="entry">
 		<h2>Page not found</h2>
 		<div class="the_content">
 		<p>Sorry we can't find that.</p>
 		<p>You could try another search or <a href="<?php bloginfo('home'); ?>">return to our homepage</a>.</p>
 		<?php get_search_form(); ?>
 		</div><!--end the_content-->
-	</article><!--end entry-->
 
 <?php endif; ?>
+
+	</article><!--end entry-->
 
 <?php get_sidebar(); ?>
 
