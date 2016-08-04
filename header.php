e1826ddba5f9b6670bc5dfaa62292012641eadfa
Nathan Gearing <natgearing@gmail.com>
2016-08-04 10:27:45 +1000

    Fix Custom Logo

--- header.php
+++ header.php
@@ -12,58 +12,50 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'ggstyle' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#main"><?php esc_html_e('Skip to content', 'ggstyle'); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-    			
-			$site_logo = get_theme_mod('custom_logo');    			
-			if($site_logo) :
-				$site_logo = wp_get_attachment_image_src( $site_logo , ‘full’ );
-			
-			    if( is_front_page() && is_home() ) :
-			    
-    			    echo "<h1 class='site-title'><a href='".esc_url(home_url('/'))."' rel='home'><img alt='".get_bloginfo('name')."' src='$site_logo[0]'></a></h1>";
-			    else :
-			    
-    			    echo "<p class='site-title'><a href='".esc_url(home_url('/'))."' rel='home'><img alt='".get_bloginfo('name')."' src='$site_logo[0]'></a></p>";
-			    
-			    endif;
-			    
-		    else :
-    			
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+    <header id="masthead" class="site-header" role="banner">
+        <div class="site-branding">
+            <?php
+            $site_logo = get_theme_mod('site_logo');
+            if ($site_logo) :
+                // Custom logo
+                if (is_front_page() || is_home()) :
+                    echo "<h1 class='site-title'><a href='".esc_url(home_url('/'))."' rel='home'><img alt='".get_bloginfo('name')."' src='$site_logo'></a></h1>";
+                else :
+                    echo "<p class='site-title'><a href='".esc_url(home_url('/'))."' rel='home'><img alt='".get_bloginfo('name')."' src='$site_logo'></a></p>";
+                endif;
+            else :
+                // No custom logo
+                if (is_front_page() || is_home()) :
+                    echo "<h1 class='site-title'><a href='".esc_url(home_url('/'))."' rel='home'>".get_bloginfo('name')."</a></h1>";
+                else :
+                    echo "<p class='site-title'><a href='".esc_url(home_url('/'))."' rel='home'>".get_bloginfo('name')."</a></p>";
+                endif;
+            endif; // $site_logo
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; 
-			
-			endif; // $site_logo
-			?>
-		</div><!-- .site-branding -->
+            $description = get_bloginfo('description', 'display');
+            if ($description || is_customize_preview()) : ?>
+                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+            <?php
+            endif;
+            ?>
+        </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ggstyle' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Menu', 'ggstyle'); ?></button>
+            <?php wp_nav_menu(array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' )); ?>
+        </nav><!-- #site-navigation -->
+    </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
