6d67c9618ffefbc3a18b897971f9573a5ad0db5e
Nathan Gearing <natgearing@gmail.com>
2016-11-22 13:31:49 +1100

    Bootstrapped

--- functions.php
+++ functions.php
@@ -3,11 +3,8 @@
  * ggstyle functions and definitions.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package ggstyle
  */
 
-if ( ! function_exists( 'ggstyle_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
@@ -15,17 +12,18 @@ if ( ! function_exists( 'ggstyle_setup' ) ) :
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function ggstyle_setup() {
+function ggstyle_setup()
+{
     /*
      * Make theme available for translation.
      * Translations can be filed in the /languages/ directory.
      * If you're building a theme based on ggstyle, use a find and replace
      * to change 'ggstyle' to the name of your theme in all the template files.
      */
-    load_theme_textdomain( 'ggstyle', get_template_directory() . '/languages' );
+    load_theme_textdomain('ggstyle', get_template_directory().'/languages');
 
     // Add default posts and comments RSS feed links to head.
-    add_theme_support( 'automatic-feed-links' );
+    add_theme_support('automatic-feed-links');
 
     /*
      * Let WordPress manage the document title.
@@ -33,62 +31,61 @@ function ggstyle_setup() {
      * hard-coded <title> tag in the document head, and expect WordPress to
      * provide it for us.
      */
-    add_theme_support( 'title-tag' );
+    add_theme_support('title-tag');
 
     /*
      * Enable support for Post Thumbnails on posts and pages.
      *
      * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
      */
-    add_theme_support( 'post-thumbnails' );
+    add_theme_support('post-thumbnails');
 
     // This theme uses wp_nav_menu() in one location.
-    register_nav_menus( array(
-        'primary' => esc_html__( 'Primary', 'ggstyle' ),
-    ) );
+    register_nav_menus(array(
+        'primary' => esc_html__('Primary', 'ggstyle'),
+    ));
 
     /*
      * Switch default core markup for search form, comment form, and comments
      * to output valid HTML5.
      */
-    add_theme_support( 'html5', array(
+    add_theme_support('html5', array(
         'search-form',
         'comment-form',
         'comment-list',
         'gallery',
         'caption',
-    ) );
+    ));
 
     /*
      * Enable support for Post Formats.
      * See https://developer.wordpress.org/themes/functionality/post-formats/
      */
-    add_theme_support( 'post-formats', array(
+    add_theme_support('post-formats', array(
         'aside',
         'image',
         'video',
         'quote',
         'link',
-    ) );
+    ));
 
     /*
      * Enable theme support Theme Logo
      * See https://codex.wordpress.org/Theme_Logo
      */
-    add_theme_support( 'custom-logo', array(
-        'height'      => 300,
-        'width'       => 300,
+    add_theme_support('custom-logo', array(
+        'height' => 300,
+        'width' => 300,
         'flex-width' => true,
-    ) );
+    ));
 
     // Set up the WordPress core custom background feature.
-    add_theme_support( 'custom-background', apply_filters( 'ggstyle_custom_background_args', array(
+    add_theme_support('custom-background', apply_filters('ggstyle_custom_background_args', array(
         'default-color' => 'ffffff',
         'default-image' => '',
-    ) ) );
+    )));
 }
-endif;
-add_action( 'after_setup_theme', 'ggstyle_setup' );
+add_action('after_setup_theme', 'ggstyle_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -97,54 +94,73 @@ add_action( 'after_setup_theme', 'ggstyle_setup' );
  *
  * @global int $content_width
  */
-function ggstyle_content_width() {
-    $GLOBALS['content_width'] = apply_filters( 'ggstyle_content_width', 640 );
+function ggstyle_content_width()
+{
+    $GLOBALS['content_width'] = apply_filters('ggstyle_content_width', 640);
 }
-add_action( 'after_setup_theme', 'ggstyle_content_width', 0 );
+add_action('after_setup_theme', 'ggstyle_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function ggstyle_widgets_init() {
-    register_sidebar( array(
-        'name'          => esc_html__( 'Sidebar', 'ggstyle' ),
-        'id'            => 'sidebar-1',
-        'description'   => esc_html__( 'Add widgets here.', 'ggstyle' ),
+function ggstyle_widgets_init()
+{
+    register_sidebar(array(
+        'name' => esc_html__('Sidebar', 'ggstyle'),
+        'id' => 'sidebar-1',
+        'description' => esc_html__('Add widgets here.', 'ggstyle'),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h2 class="widget-title">',
-        'after_title'   => '</h2>',
-    ) );
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ));
 }
-add_action( 'widgets_init', 'ggstyle_widgets_init' );
+add_action('widgets_init', 'ggstyle_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function ggstyle_scripts() {
-    wp_enqueue_style( 'ggstyle-style', get_stylesheet_uri() );
+function ggstyle_scripts()
+{
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri().'/css/bootstrap.css');
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Slab:400,700|Roboto:300,300i,400,400i,700,700i');
+    wp_enqueue_style('ggstyle-style', get_stylesheet_uri());
 
-    wp_enqueue_script( 'ggstyle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-    wp_enqueue_script( 'ggstyle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
+    if (!wp_script_is('jquery', 'enqueued')) {
+        wp_enqueue_script('jquery');
     }
+
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri().'/js/bootstrap.js', array(), '3.3.7', true);
+
+    wp_enqueue_script('ggstyle-navigation', get_template_directory_uri().'/js/navigation.js', array(), '20151215', true);
+
+    wp_enqueue_script('ggstyle-skip-link-focus-fix', get_template_directory_uri().'/js/skip-link-focus-fix.js', array(), '20151215', true);
+
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
+
+    wp_enqueue_script('theme-js', get_template_directory_uri().'/js/theme.js', array(), '0.0.1', true);
 }
-add_action( 'wp_enqueue_scripts', 'ggstyle_scripts' );
+add_action('wp_enqueue_scripts', 'ggstyle_scripts');
 
-
-function ggstyle_remove_menus() {
-  remove_menu_page( 'edit-comments.php' );          //Comments
-  remove_menu_page( 'link-manager.php' );           //Links
+/**
+ * Remove old admin pages.
+ */
+function ggstyle_remove_menus()
+{
+    remove_menu_page('edit-comments.php');          //Comments
+    remove_menu_page('link-manager.php');           //Links
 }
-add_action( 'admin_menu', 'ggstyle_remove_menus' );
+add_action('admin_menu', 'ggstyle_remove_menus');
 
-
-function ggstyle_change_post_menu_label() {
+/**
+ * Change posts label.
+ */
+function ggstyle_change_post_menu_label()
+{
     global $menu;
     global $submenu;
     $menu[5][0] = 'News';
@@ -152,54 +168,54 @@ function ggstyle_change_post_menu_label() {
     $submenu['edit.php'][10][0] = 'Add News';
     echo '';
 }
-function ggstyle_change_post_object_label() {
-        global $wp_post_types;
-        $labels = &$wp_post_types['post']->labels;
-        $labels->name = 'News';
-        $labels->singular_name = 'News';
-        $labels->add_new = 'Add News';
-        $labels->add_new_item = 'Add News';
-        $labels->edit_item = 'Edit News';
-        $labels->new_item = 'News';
-        $labels->view_item = 'View News';
-        $labels->search_items = 'Search News';
-        $labels->not_found = 'No News found';
-        $labels->not_found_in_trash = 'No News found in Trash';
+function ggstyle_change_post_object_label()
+{
+    global $wp_post_types;
+    $labels = &$wp_post_types['post']->labels;
+    $labels->name = 'News';
+    $labels->singular_name = 'News';
+    $labels->add_new = 'Add News';
+    $labels->add_new_item = 'Add News';
+    $labels->edit_item = 'Edit News';
+    $labels->new_item = 'News';
+    $labels->view_item = 'View News';
+    $labels->search_items = 'Search News';
+    $labels->not_found = 'No News found';
+    $labels->not_found_in_trash = 'No News found in Trash';
 }
-add_action( 'init', 'ggstyle_change_post_object_label' );
-add_action( 'admin_menu', 'ggstyle_change_post_menu_label' );
+add_action('init', 'ggstyle_change_post_object_label');
+add_action('admin_menu', 'ggstyle_change_post_menu_label');
 
-/** 
- * Put GG link on Wp-admin footer
+/**
+ * Put GG link on Wp-admin footer.
  */
- 
- function remove_footer_admin () {
-  echo 'Site made proudly by <a href="http://www.greengraphics.com.au">Greengraphics</a>.';
+function remove_footer_admin()
+{
+    echo 'Site made proudly by <a href="http://www.greengraphics.com.au">Greengraphics</a>.';
 }
 add_filter('admin_footer_text', 'remove_footer_admin');
-
 
 /**
  * Implement the Custom Header feature.
  */
-require get_template_directory() . '/inc/custom-header.php';
+require get_template_directory().'/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.php';
+require get_template_directory().'/inc/template-tags.php';
 
 /**
  * Custom functions that act independently of the theme templates.
  */
-require get_template_directory() . '/inc/extras.php';
+require get_template_directory().'/inc/extras.php';
 
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
+require get_template_directory().'/inc/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
  */
-require get_template_directory() . '/inc/jetpack.php';
+require get_template_directory().'/inc/jetpack.php';
